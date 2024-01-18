412a78b0db654335c0a95d2fa2df95b3a72adcca
Ashish Shah <133593891+dotAshishah@users.noreply.github.com>
2024-01-18 20:41:07 +0530

    Create 5th.java

---
+++ 5th.java
@@ -0,0 +1,18 @@
+// Use of variables
+
+public class Myclass
+{
+public static void main(String[] args)
+{
+String sName;
+sName="Harish";
+int age=41;
+System.out.println("Hello "+sName);
+System.out.println("Your age before update is "+age);
+age=42;
+System.out.println("Hello "+sName);
+System.out.println("Your age after update is "+age);
+
+}
+
+}
